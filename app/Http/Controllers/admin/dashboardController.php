a34f04e397487eb8c528a808351906ddec56be99
SafnaPrasetiono <safnaprasetiono71@gmail.com>
2021-05-05 21:19:41 +0700

    Menghapus file homeController dan menambahkan file dashboardController

--- app/Http/Controllers/admin/dashboardController.php
+++ app/Http/Controllers/admin/dashboardController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class homeController extends Controller
+class dashboardController extends Controller
 {
     //
 }
