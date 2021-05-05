068d89b56f7b70c68d3b6fec172df1637ff6d801
SafnaPrasetiono <safnaprasetiono71@gmail.com>
2021-05-05 21:57:32 +0700

    Merubah file home pada admin

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,17 @@
+@extends('layout.panel')
+
+@section('title')
+    <title>Seblak Gerah - Dashboard</title>
+@endsection
+
+@section('mainpages')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="d-block rounded shadow p-3">
+                    <h2>Hello, Wellcome to admin dashboard</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
